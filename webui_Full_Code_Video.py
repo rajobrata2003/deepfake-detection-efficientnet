56c8fd1ec3ab1bf80d5e88a4bfe82c4fd23191b1
rajobrata2003 <35872390+rajobrata2003@users.noreply.github.com>
2024-04-07 01:57:13 +0530

    Update webui_Full_Code_Video.py

--- webui_Full_Code_Video.py
+++ webui_Full_Code_Video.py
@@ -1,10 +1,69 @@
-import torch
+from __future__ import annotations
+from typing import Iterable
 import gradio as gr
+from gradio.themes.base import Base
+from gradio.themes.utils import colors, fonts, sizes
+import torch
 import cv2
+import random
 import numpy as np
 from mtcnn import MTCNN
 from efficientnet_pytorch import EfficientNet
-import random
+
+
+# Custom theme class for jet black and blood red theme
+class JetBlackBloodRed(Base):
+    def __init__(
+        self,
+        *,
+        primary_hue: colors.Color | str = colors.red,
+        secondary_hue: colors.Color | str = colors.cyan,
+        neutral_hue: colors.Color | str = colors.zinc,
+        spacing_size: sizes.Size | str = sizes.spacing_md,
+        radius_size: sizes.Size | str = sizes.radius_md,
+        text_size: sizes.Size | str = sizes.text_lg,
+        font: fonts.Font
+        | str
+        | Iterable[fonts.Font | str] = (
+            fonts.GoogleFont("Quicksand"),
+            "ui-sans-serif",
+            "sans-serif",
+        ),
+        font_mono: fonts.Font
+        | str
+        | Iterable[fonts.Font | str] = (
+            fonts.GoogleFont("IBM Plex Mono"),
+            "ui-monospace",
+            "monospace",
+        ),
+    ):
+        super().__init__(
+            primary_hue=primary_hue,
+            secondary_hue=secondary_hue,
+            neutral_hue=neutral_hue,
+            spacing_size=spacing_size,
+            radius_size=radius_size,
+            text_size=text_size,
+            font=font,
+            font_mono=font_mono,
+        )
+        super().set(
+            #body_background_fill="#BED7DC",
+            body_background_fill_dark="#88AB8E",
+            #button_primary_background_fill="#E5DDC5",
+            #button_primary_background_fill_hover="linear-gradient(90deg, *primary_200, *secondary_300)",
+            button_primary_text_color_dark="#88AB8E",
+            button_secondary_text_color_dark="#88AB8E",
+            button_primary_background_fill_dark="#F2F1EB",
+            button_secondary_background_fill_dark="#EEE7DA",
+            # slider_color="*secondary_300",
+            # slider_color_dark="*secondary_600",
+            block_title_text_weight="600",
+            block_border_width="3px",
+            block_shadow="*shadow_drop_lg",
+            button_shadow="*shadow_drop_lg",
+            button_large_padding="32px",)
+
 
 detector = MTCNN()
 
@@ -66,6 +125,6 @@ def predict_video(video):
     else:
         return "Prediction: Uncertain"
 
-iface = gr.Interface(predict_video, inputs="video", outputs="text", title="Deepfake Detection on Video")
+iface = gr.Interface(predict_video, inputs="video", outputs="text", title="Deepfake Detection on Video",theme=JetBlackBloodRed())
 iface.launch(share=True, debug=True)
 
